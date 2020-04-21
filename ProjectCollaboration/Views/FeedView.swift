c94da7c766809fe7d59aa81d9d3d246eaa84fa19
Tsering Lama <tseringlama@pursuit.org>
2020-04-21 13:07:53 -0400

    initial feed UI

--- ProjectCollaboration/Views/FeedView.swift
+++ ProjectCollaboration/Views/FeedView.swift
@@ -9,13 +9,56 @@
 import UIKit
 
 class FeedView: UIView {
+    
+    public lazy var feedCV: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let cv = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        cv.backgroundColor = .systemRed
+        return cv
+    }()
+    
+    public lazy var searchBar: UISearchBar = {
+       let searchBar = UISearchBar()
+        searchBar.backgroundColor = .systemBlue
+       return searchBar
+    }()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        setUpCV()
+        setupSearch()
+    }
+    
+    private func setUpCV() {
+        addSubview(feedCV)
+        feedCV.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            feedCV.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 55),
+            feedCV.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            feedCV.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            feedCV.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        ])
+    }
+    
+    private func setupSearch() {
+        addSubview(searchBar)
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 0),
+            searchBar.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            searchBar.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: 0)
+        ])
+    }
+
 
 }
